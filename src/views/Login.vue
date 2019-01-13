e2d95f6c89655653e2778ef5c2c89667697344a6
phat-emurgovn <39687316+phat-emurgovn@users.noreply.github.com>
2019-01-13 14:09:57 +0700

    Update logo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,26 +7,36 @@
             <v-card class="elevation-1 pa-3">
               <v-card-text>
                 <div class="layout column align-center">
-                  <img src="/static/m.png" alt="Vue Material Admin" width="120" height="120">
-                  <h1 class="flex my-4 primary--text">Material Admin Template</h1>
-                </div>                
+                  <img
+                    src="/static/logo-act.png"
+                    alt="ACT Payment"
+                    width="120"
+                    height="120"
+                  />
+                  <h1 class="flex my-4 primary--text">ACT Payment</h1>
+                </div>
                 <v-form>
-                  <v-text-field append-icon="person" name="login" label="Login" type="text" v-model="model.username"></v-text-field>
-                  <v-text-field append-icon="lock" name="password" label="Password" id="password" type="password" v-model="model.password"></v-text-field>
+                  <v-text-field
+                    append-icon="person"
+                    name="login"
+                    label="Login"
+                    type="text"
+                    v-model="model.username"
+                  ></v-text-field>
+                  <v-text-field
+                    append-icon="lock"
+                    name="password"
+                    label="Password"
+                    id="password"
+                    type="password"
+                    v-model="model.password"
+                  ></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
-                <v-btn icon>
-                  <v-icon color="blue">fa fa-facebook-square fa-lg</v-icon>
+                <v-btn block color="primary" @click="login" :loading="loading"
+                  >Login
                 </v-btn>
-                <v-btn icon>
-                  <v-icon color="red">fa fa-google fa-lg</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon color="light-blue">fa fa-twitter fa-lg</v-icon>
-                </v-btn>
-                <v-spacer></v-spacer>
-                <v-btn block color="primary" @click="login" :loading="loading">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -41,24 +51,23 @@ export default {
   data: () => ({
     loading: false,
     model: {
-      username: 'admin@isockde.com',
-      password: 'password'
+      username: "",
+      password: ""
     }
   }),
 
   methods: {
-    login () {
+    login() {
       this.loading = true;
       setTimeout(() => {
-        this.$router.push('/dashboard');
+        this.$router.push("/dashboard");
       }, 1000);
     }
   }
-
 };
 </script>
 <style scoped lang="css">
-  #login {
+#login {
     height: 50%;
     width: 100%;
     position: absolute;
@@ -66,5 +75,5 @@ export default {
     left: 0;
     content: "";
     z-index: 0;
-  }
+}
 </style>
